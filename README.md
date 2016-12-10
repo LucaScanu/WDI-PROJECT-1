52324afbe0dc2bb08e764fcc1b32d10cedf6ba24
LucaScanu <lucagscanu@gmail.com>
2016-12-10 11:12:37 +0000

    Started pseudo code for Project1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WDI_PROJECT_1
 First project for my web development immersive course at GA
+FLIP IT AND MATCH 
--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-console.log('blue');
+/* Pseudo Code
+-The aim of the project is to create a memory game of flipping tiles to be paired against time. The game will have three different levels of difficulty (easy, medium, hard) and the option to compete against a friend.
+-The game board will be a 6X6 grid and the tiles will have pictures of famous cartoon characters and associated sounds.
+-The player will have to click on the difficulty level to start the game or click to play against another player.
+-On start will have to click on  each tile/card to flip them and reveal a picture to pair it with another tile with the same picture on it.
+-If the pair is successfull the tiles with the paired pictures will disappear until no tiles remain in the board.
+-If the pair is unsuccesful the tiles will flip back to their original status.
+-If single play is selected  the player will have a set time to finish the game, when the time is finished the play is halted.
+-If the player goes against another player there won't be any time limit and the player that finishes first will win.
+-BASIC GAME:
+-create a 6 x 6 grid to display the game board.
+-each cell of the grid corresponds to one of cards face down.
+-create the cards to be used in the game
+-shuffle the cards randomly into the game board
+-the user should be able to click on only two different cards each play.
+-the cards should flip on click.
+-the cards with the same picture are 'paired' and should disappear from the board
+-the cards that are not paired should flip back and available to be clicked again
+-The game is finished when all cards are cleared from the board
+COMPLICATIONS:
+-add a timer to the game
+-the player will have to clear the board in a set time
+-the last ten seconds the timer will pulse red
+-once the time has ran out the game will stop and a 'game over' message will appear on the screen
+-add 'easy', 'medium', 'hard' levels of difficulty
+-add the option of two player playing gainst each other
+-add sounds to play when cards are paired
+-display a winning message at the end of the game
+-add a counter to keep track of best times, how many games won and how many games lost.
+-finally style the page
+
+
+
+
+
+
+
+
+*/
