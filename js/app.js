b65ae14947bc65bd83f317db4ff93e486eb5c3aa
LucaScanu <lucagscanu@gmail.com>
2016-12-12 21:46:49 +0000

    Monday Evening

--- js/app.js
+++ js/app.js
@@ -1,125 +1,119 @@
-/* Pseudo Code
--The aim of the project is to create a memory game of flipping tiles to be paired against time. The game will have three different levels of difficulty (easy, medium, hard) and the option to compete against a friend.
--The game board will be a 6X6 grid and the tiles will have pictures of famous cartoon characters and associated sounds.
--The player will have to click on the difficulty level to start the game or click to play against another player.
--On start will have to click on  each tile/card to flip them and reveal a picture to pair it with another tile with the same picture on it.
--If the pair is successfull the tiles with the paired pictures will disappear until no tiles remain in the board.
--If the pair is unsuccesful the tiles will flip back to their original status.
--If single play is selected  the player will have a set time to finish the game, when the time is finished the play is halted.
--If the player goes against another player there won't be any time limit and the player that finishes first will win.
--BASIC GAME:
--create a 6 x 6 grid to display the game board.
--each cell of the grid corresponds to one of cards face down.
--create an array of images
--assign a pair of each picture to the cards array for a total of 36 pictures
--assign each picture randomly to each square of the game board
-- win logic: if the players clicks and pairs two equal images alert player
-- alert('pair matched')
-- if the players clicks on two different images alert message ('no match')
--the user should be able to click on only two different cards each play.
--the cards should flip on click.
--the cards with the same picture are 'paired' and should disappear from the board
--the cards that are not paired should flip back and available to be clicked again
--The game is finished when all cards are cleared from the board
-COMPLICATIONS:
--add a timer to the game
--the player will have to clear the board in a set time
--the last ten seconds the timer will pulse red
--once the time has ran out the game will stop and a 'game over' message will appear on the screen
--add 'easy', 'medium', 'hard' levels of difficulty
--add the option of two player playing gainst each other
--add sounds to play when cards are paired
--display a winning message at the end of the game
--add a counter to keep track of best times, how many games won and how many games lost.
--finally style the page
-
-*/
-
-// create a Game object
+// created the Game object
 
 var Game = Game || {};
 
-Game.boardBase = 6;
-Game.cards = [];
-Game.board;
-Game.square;
-
-// created an array of 18 images
-
-var deck = [
-  '/images/Bart1.jpeg',
-  '/images/Bart2.jpeg',
-  '/images/Brian1.jpeg',
-  '/images/Brian2.jpeg',
-  '/images/Cleveland.jpeg',
-  '/images/Fredflinstone.jpeg',
-  '/images/Homer2.jpeg',
-  '/images/Homers1.jpeg',
-  '/images/PeterG.jpeg',
-  '/images/PeterG2.jpeg',
-  '/images/Popeye.jpeg',
-  '/images/Quagmire3.jpeg',
-  '/images/Quagmire4.jpeg',
-  '/images/Roger1.jpeg',
-  '/images/Roger2.jpeg',
-  '/images/Rollo.jpeg',
-  '/images/Stewie.jpeg',
-  '/images/Stewie2.jpeg'
+Game.cardImages = [
+  'Bart1',
+  'Bart2',
+  'Brian1',
+  'Brian2',
+  'Cleveland',
+  'Fredflinstone',
+  'Homer2',
+  'Homers1',
+  'PeterG',
+  'PeterG2',
+  'Popeye',
+  'Quagmire3',
+  'Quagmire4',
+  'Roger1',
+  'Roger2',
+  'Rollo',
+  'Stewie',
+  'Stewie2'
 ];
 
-// this for loop will push twice the cards in our deck into the Game.cards array
+Game.chosenCards = [];
 
-for (var i = 1; i < 19; i++) {
-  var randomCards = Math.floor(Math.random(deck.length));
-  var playCards = deck[randomCards];
-  Game.cards.push(playCards);
-  Game.cards.push(playCards);
-  deck.splice(randomCards, 1);
-}
-// This sort function will sort the cards randomly
-
-Game.cards.sort(function() {
-  return 0.5 - Math.random();
-});
-
-
-
-// console.log(Game.cards);
-
-// create gameboard with jquery
-
-Game.createBoard = function() {
-  var $body = $('body');
-  var $board = $(document.createElement('div')).addClass('board');
-  $body.append($board);
-  this.createSquare($board);
+Game.twoCardsChosen = function twoCardsChosen() {
+  return Game.chosenCards.length === 2;
 };
 
-// create the card(div) elements and two div for the card front and back
-// append the cards to the board
-Game.createSquare = function($board) {
-  $(Game.cards).each(function(index, element) {
-    $board.append('<div class="square"><div class="front"></div><div class="back"></div></div>');
-  });
-  this.addCardValues();
+Game.compareCards = function compareCards() {
+  var firstCard  = Game.chosenCards[0];
+  var secondCard = Game.chosenCards[1];
+
+  return firstCard.children('.back').css('background-image') === secondCard.children('.back').css('background-image');
 };
 
-Game.addCardValues = function() {
-  $('.square').each(function(index, element) {
-    $(element).find('.back').append('<img src=".' + Game.cards[index] + '">');
-    // console.log(Game.cards[index]);
-  });
-  this.checkForWin();
+Game.selectCard = function selectCard() {
+  // The card we chose
+  var card = $(this);
+  // Store the card for later use...
+  Game.chosenCards.push(card);
+
+  // Find the index of that card from the cardsArray
+  var indexOfSquare = Game.cardsArray.indexOf(this);
+  // Choose the corresponding image from this.deck using the same index
+  var image         = Game.deck[indexOfSquare];
+  console.log(indexOfSquare);
+  // Add the image to the .back of the card that we chose
+  var imageUrl      = './images/' + image + '.jpeg';
+  card.children('.back').css('background-image', 'url("'+ imageUrl +'")');
+  console.log('url("'+ imageUrl +'")');
+  // Flip the card
+  card.toggleClass('flipped');
+  // Remove the eventListener so we can't click on it again
+  card.off('click', this.selectCard);
+
+  if (!Game.twoCardsChosen()) {
+    // If only one card has been clicked
+    return false;
+  } else if (Game.twoCardsChosen() && Game.compareCards()) {
+    // If two cards have been chosen check for a match with the
+    // Game.compareCards function
+    Game.chosenCards = [];
+    return false;
+  } else {
+    // No match
+    Game.chosenCards.forEach(function(chosenCard) {
+      setTimeout(function(){
+        chosenCard.toggleClass('flipped');
+        chosenCard.css('background-image', 'none');
+        // Re-adding the click eventHandler
+        chosenCard.on('click', Game.selectCard);
+      }, 1500);
+    });
+    Game.chosenCards = [];
+  }
 };
 
-Game.checkForWin = function() {
-  $('.back').on('click', function() {
-    console.log('clicked');
-  });
+Game.dealCards = function dealCards() {
+  // append a three div section for each card to the Game.board and give them
+  // a class='card' for the card div and ='front', ='back' for the nested divs
+  var i = 0;
+  for (i; i < this.deck.length; i++) {
+    var $card = $('<div class="card"><div class="front"></div><div class="back"></div></div>');
+    this.$board.append($card);
+  }
+  // Select all of the cards that we added to the board
+  this.$cards = $('.card');
+  // Convert the HTMLCollection to an array (so we can use .indexOf)
+  this.cardsArray = [].slice.call(this.$cards);
+  // Add a click eventListener to the card and call the function selectCard
+  this.$cards.on('click', this.selectCard);
 };
 
-$(start);
+Game.shuffle = function shuffle() {
+  // Create an array with 2 of each image using concatenation
+  var array = this.cardImages.concat(this.cardImages);
+  // Shuffle this array with the Durstenfeld shuffle algorithm
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
 
-function start() {
-  Game.createBoard();
-}
+Game.start = function start() {
+  this.base   = 6;
+  this.$body  = $('body');
+  this.$board = $('<div class="board"></div>');
+  this.$body.append(this.$board);
+  this.deck   = this.shuffle();
+  this.dealCards();
+};
+
+$(Game.start.bind(Game));
