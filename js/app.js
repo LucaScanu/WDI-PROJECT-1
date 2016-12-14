070f55b72981819dd420477fe786e4576b3cd115
LucaScanu <lucagscanu@gmail.com>
2016-12-14 09:38:00 +0000

    Wednesday morning updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,50 @@
-// created the Game object
+// /* Pseudo Code
+// -The aim of the project is to create a memory game of flipping tiles to be paired against time. The game will have three different levels of difficulty (easy, medium, hard) and the option to compete against a friend.
+// -The game board will be a 6X6 grid and the tiles will have pictures of cartoon characters and associated sounds.
+// -The player will have to click on the difficulty level to start the game or click to play against another player.
+// -On start will have to click on  each tile/card to flip them and reveal a picture to pair it with another tile with the same picture on it.
+// -If the pair is successfull the tiles with the paired pictures will disappear until no tiles remain in the board.
+// -If the pair is unsuccesful the tiles will flip back to their original status.
+// -If single play is selected  the player will have a set time to finish the game, when the time is finished the play is halted.
+// -If the player goes against another player there won't be any time limit and the player that finishes first will win.
+// -BASIC GAME:
+// -create a 6 x 6 grid to display the game board.
+// -each cell of the grid corresponds to one of cards face down.
+// -create an array of images
+// -assign a pair of each picture to the cards array for a total of 36 pictures
+// -assign each picture randomly to each square of the game board
+// - win logic: if the players clicks and pairs two equal images alert player
+// - alert('pair matched')
+// - if the players clicks on two different images alert message ('no match')
+// -the user should be able to click on only two different cards each play.
+// -the cards should flip on click.
+// -the cards with the same picture are 'paired' and should disappear from the board
+// -the cards that are not paired should flip back and available to be clicked again
+// -The game is finished when all cards are cleared from the board
+// COMPLICATIONS:
+// -add a timer to the game
+// -the player will have to clear the board in a set time
+// -the last ten seconds the timer will pulse red
+// -once the time has ran out the game will stop and a 'game over' message will appear on the screen
+// -add 'easy', 'medium', 'hard' levels of difficulty
+// -add the option of two player playing gainst each other
+// -add sounds to play when cards are paired
+// -display a winning message at the end of the game
+// -add a counter to keep track of best times, how many games won and how many games lost.
+// -finally style the page
+//
+// */
+
+
+// this function will toggle the instruction menu
 
 $(document).ready(function(){
   $('#inst').click(function(){
     $('p').slideToggle(1200);
   });
 });
+
+// created the Game object
 
 var Game = Game || {};
 
@@ -31,7 +71,6 @@ Game.cardImages = [
 
 Game.chosenCards = [];
 
-
 Game.twoCardsChosen = function twoCardsChosen() {
   return Game.chosenCards.length === 2;
 };
@@ -48,6 +87,7 @@ Game.selectCard = function selectCard() {
   var card = $(this);
   // Store the card for later use...
   Game.chosenCards.push(card);
+
 
   // Find the index of that card from the cardsArray
   var indexOfSquare = Game.cardsArray.indexOf(this);
@@ -73,9 +113,6 @@ Game.selectCard = function selectCard() {
         chosenCard.empty('slow');
       }, 1000);
     });
-    // Play sounds
-    // var audio = new Audio('./sounds/doh1.');
-    // audio.play();
 
     Game.chosenCards = [];
 
@@ -88,11 +125,8 @@ Game.selectCard = function selectCard() {
         // Re-adding the click eventHandler
         chosenCard.on('click', Game.selectCard);
       }, 1500);
-      // Play sounds
-      // var audio = new Audio('./sounds/doh1.wav');
-      // audio.play();
+      Game.chosenCards = [];
     });
-    Game.chosenCards = [];
   }
 };
 
